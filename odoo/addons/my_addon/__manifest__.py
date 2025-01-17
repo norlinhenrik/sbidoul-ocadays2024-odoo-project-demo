0593eca25b3a9c9a8ccdd6ca371f30f661c58049
Henrik Norlin <henrik.norl@gmai.com>
2025-01-17 11:54:12 +0100

    16.0.py39 without mis_builder_budget (depends on py310 or higher)

--- odoo/addons/my_addon/__manifest__.py
+++ odoo/addons/my_addon/__manifest__.py
@@ -1,10 +1,10 @@
 {
     "name": "My Addon",
     "version": "16.0.1.0.0",
-    "depends": [
-        "mis_builder_budget",
-    ],
-    "external_dependencies": {
-        "python": ["pydantic>=2"],
-    }
+    # "depends": [
+    #     "mis_builder_budget",
+    # ],
+    # "external_dependencies": {
+    #     "python": ["pydantic>=2"],
+    # }
 }
